e1cb266c1edf4c25b0d9a11daf018d5200d6fcf3
Sam Phillips <samdphillips@gmail.com>
2023-03-19 15:37:30 -0700

    Update deps.

--- boato-sigv4-lib/info.rkt
+++ boato-sigv4-lib/info.rkt
@@ -3,6 +3,7 @@
 (define name "boato-sigv4-lib")
 (define collection "boato")
 (define version "2023.3.17")
-(define deps '("base"))
+(define deps
+  '("base" "crypto-lib" "typed-racket-lib" "typed-racket-more"))
 (define pkg-authors '(samdphillips@gmail.com))
 (define license 'Apache-2.0)
