6b1676cb3d5d6eda4dbd61a5f5bd9b25d7aba9b0
Sam Phillips <samdphillips@gmail.com>
2023-03-24 11:38:38 -0700

    Add gregor-lib to deps

--- boato-lang-lib/info.rkt
+++ boato-lang-lib/info.rkt
@@ -3,6 +3,6 @@
 (define name "boato-lang-lib")
 (define collection "boato")
 (define version "2023.3.17")
-(define deps '("base"))
+(define deps '("base" "gregor-lib"))
 (define pkg-authors '(samdphillips@gmail.com))
 (define license 'Apache-2.0)
