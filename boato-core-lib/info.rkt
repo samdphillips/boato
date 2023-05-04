de9931763ad14498fe006202d3ee1446d5c241f1
Sam Phillips <samdphillips@gmail.com>
2023-05-04 14:15:51 -0700

    Move `boato-lang-lib` -> `boato-core-lib`

--- boato-core-lib/info.rkt
+++ boato-core-lib/info.rkt
@@ -3,6 +3,6 @@
 (define name "boato-core-lib")
 (define collection "boato")
 (define version "2023.3.17")
-(define deps '("base"))
+(define deps '("base" "gregor-lib" "http-easy-lib"))
 (define pkg-authors '(samdphillips@gmail.com))
 (define license 'Apache-2.0)
--- boato-lang-lib/info.rkt
+++
@@ -1,8 +0,0 @@
-#lang info
-
-(define name "boato-lang-lib")
-(define collection "boato")
-(define version "2023.3.17")
-(define deps '("base" "gregor-lib"))
-(define pkg-authors '(samdphillips@gmail.com))
-(define license 'Apache-2.0)
